87998d85a0794af06dc72abbf1a38b2844ccba53
Urszula Subkowska <urszula.subkowska@sabre.com>
2023-07-30 09:53:48 +0000

    initial commit

---
+++ src/main/java/com/example/springcloudtask/SpringCloudTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTaskApplication.class, args);
+    }
+
+}
