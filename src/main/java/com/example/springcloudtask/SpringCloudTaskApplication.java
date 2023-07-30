5e75792c22e295bf3e4a9cc14367997ddc61c145
Urszula Subkowska <urszula.subkowska@sabre.com>
2023-07-30 10:17:48 +0000

    helloWorld task

--- src/main/java/com/example/springcloudtask/SpringCloudTaskApplication.java
+++ src/main/java/com/example/springcloudtask/SpringCloudTaskApplication.java
@@ -1,13 +1,33 @@
 package com.example.springcloudtask;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableTask
 public class SpringCloudTaskApplication {
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return new SpringCloudTaskRunner();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudTaskApplication.class, args);
+    }
+
+
+    public static class SpringCloudTaskRunner implements ApplicationRunner {
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            System.out.println("Hello, Spring Cloud Task!");
+
+        }
     }
 
 }
